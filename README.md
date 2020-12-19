65479f55772338abce020fcdce08f9de3fdd06b9
pmhalvor <pmchalvorsen@gmail.com>
2020-12-19 18:11:40 +0000

    figure oput how to show on site

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NLP projects
 
+## /highlitgher
+This application scrapes Google's S.R.E. book from [https://sre.google/sre-book](https://sre.google/sre-book), then highlights the dependencies using SpaCy's dependency parser. The easiest way to use is to run the [Site Reliability Engineering (highlighted)](#highlighter/Site-Reliability-Engineering-(highlighted).ipynb) notebook. 
+
+
+## /spacy
+
+SpaCy is an open source text analysis library for Python. This directory shows some examples of how to use it. 
+
+
--- highlighter/color_chunks.py
+++ highlighter/color_chunks.py
@@ -1,10 +1,6 @@
 import spacy
 from termcolor import colored
 
-str1 = "In a typical application, not all requests are equal: failing a new user sign-up request is different from failing a request polling for new email in the background."
-str2 = " In many cases, however, availability calculated as the request success rate over all requests is a reasonable approximation of unplanned downtime, as viewed from the end-user perspective."
-str3 = " When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
-
 
 def get_colored_chunks(text, markdown=False, color='green', norwegian=False):
     ###### EXPORT THIS TO STATE OF CLASS ######
@@ -33,9 +29,9 @@ def get_colored_chunks(text, markdown=False, color='green', norwegian=False):
                     pre = '# '
                     chunk_text = chunk_text.strip(('# '))
                 
-                replacement = pre+f'<mark><span style="color:{color}">{chunk_text}</span></mark>'
+                replacement = pre+f'<mark><span style="color:{color}">{chunk_text} </span></mark>'
             else:
-                replacement = pre+colored(chunk_text, color, 'on_yellow')
+                replacement = colored(chunk.text, color, 'on_yellow')
             ###########################################
 
 
@@ -47,5 +43,9 @@ def get_colored_chunks(text, markdown=False, color='green', norwegian=False):
 
 
 if __name__=='__main__':
+    str1 = "In a typical application, not all requests are equal: failing a new user sign-up request is different from failing a request polling for new email in the background."
+    str2 = " In many cases, however, availability calculated as the request success rate over all requests is a reasonable approximation of unplanned downtime, as viewed from the end-user perspective."
+    str3 = " When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
+
     text = str1+str2
     print(get_colored_chunks(text))
